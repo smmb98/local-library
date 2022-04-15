a89ab794dd656ba69363556b3caccd25e7187d52
Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2022-04-15 06:15:17 +0500

    updated

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(compression());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
